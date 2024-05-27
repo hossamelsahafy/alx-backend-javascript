dadc2ef4edeef7a3466d53e0bf9d2686e5b2129d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-05-28 02:53:26 +0300

    File

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,5 @@
-export default function taskBlock(trueOrFalse) {
+export default function taskBlock() {
   const task = false;
   const task2 = true;
-
-  return [task, task2];
+    return [task, task2];
 }
