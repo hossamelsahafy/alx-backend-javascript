01d619b06cfa062bae43e867c726b2e93be9a2b9
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-05-28 05:02:31 +0300

    Files

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,8 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars()  {
-      return `${this.income}`;
+    getIncomeInDollars() {
+      return `$${this.income}`;
     },
     getIncomeInEuros() {
       return `${this.income} euros`;
