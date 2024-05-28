e991961f520e178f3b41cf6b13271cfc92a86d35
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-05-28 03:00:06 +0300

    File

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,5 +1,11 @@
-export default function taskBlock() {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
-    return [task, task2];
+
+  if (trueOrFalse) {
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
+  }
+
+  return [task, task2];
 }
