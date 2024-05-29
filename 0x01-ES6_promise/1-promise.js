015a7964682d1b0bfce5cf9f6dfb5d3c77bbadb6
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-05-29 07:15:11 +0300

    Promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(Error('The fake API is not working currently'));
+    }
+  });
+}
+export default getResponseFromAPI;
