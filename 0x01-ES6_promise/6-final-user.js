e812e7345b3efb10f4260810b30543b6116b1430
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-05-29 09:45:34 +0300

    File

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,7 @@ function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => res.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
     })));
 }
 export default handleProfileSignup;
