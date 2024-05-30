e8474f103bfdb6e5baa2317902e59eb4a37f620f
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-05-31 01:15:44 +0300

    Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+}
+
+export default Building;
