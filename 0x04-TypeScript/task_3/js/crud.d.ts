eda2e71020a198059a188be5fe96703dd1ade04b
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-06-05 20:33:28 +0300

    Files

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,7 @@
+declare module 'crud' {
+    import { RowID, RowElement } from './interface';
+  
+    export function insertRow(row: RowElement): RowID;
+    export function deleteRow(rowId: RowID): void;
+    export function updateRow(rowId: RowID, row: RowElement): RowID;
+  }
