3d92698f870d9ca8942c6d9385b718d75adefc72
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-06-05 03:07:35 +0300

    File

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buf = new ArrayBuffer(length);
+  let view = new DataView(buf);
+  view.setInt8(position, value);
+  return view;
+}
+
+export default createInt8TypedArray;
