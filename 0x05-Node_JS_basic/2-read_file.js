7735dabfc0b249bdd73fa56e1113d830cfb153ea
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-23 00:03:48 +0300

    File

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter(line => line.trim() !== ''); // Remove empty lines
+    const lines = data.split('\n').filter(line => line.trim() !== '');
 
     if (lines.length <= 1) {
       console.log('Number of students: 0');
