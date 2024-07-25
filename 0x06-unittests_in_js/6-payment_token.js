aa8857eba5fe0eebf3a5b7a70da0f03efe9bb830
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-25 19:56:47 +0300

    Files

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,10 +1,7 @@
-// 6-payment_token.js
-function getPaymentTokenFromAPI(success) {
-    return new Promise((resolve, reject) => {
-      if (success) {
-        resolve({ data: 'Successful response from the API' });
-      }
-    });
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+  if (success) {
+    resolve({data: 'Successful response from the API'});
   }
-  
-  module.exports = getPaymentTokenFromAPI;
+});
+
+module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,25 +2,10 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('should return a successful response when success is true', (done) => {
+  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
     getPaymentTokenFromAPI(true)
-      .then((response) => {
-        try {
-          expect(response).to.deep.equal({ data: 'Successful response from the API' });
-          done();
-        } catch (error) {
-          done(error);
-        }
-      })
-      .catch(done);
-  });
-
-  it('should not resolve when success is false', (done) => {
-    getPaymentTokenFromAPI(false)
-      .then(() => {
-        done(new Error('Expected promise to be rejected or to not resolve'));
-      })
-      .catch(() => {
+      .then((res) => {
+        expect(res).to.deep.equal({data: 'Successful response from the API'});
         done();
       });
   });
