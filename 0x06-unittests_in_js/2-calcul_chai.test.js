4a6aa89785dad839691a98c385e62f79e744923b
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-25 18:31:59 +0300

    Files

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,44 +1,44 @@
-const assert = require('assert');
-const calculateNumber = require('./1-calcul.js');
+const { expect } = require('chai');
+const calculateNumber = require('./2-calcul_chai.js');
 
 describe('calculateNumber', () => {
   describe('SUM', () => {
     it('should return 5 when inputs are 1 and 3.7', () => {
-      assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
+      expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
     });
 
     it('should return 6 when inputs are 1.5 and 3.7', () => {
-      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+      expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
     });
   });
 
   describe('SUBTRACT', () => {
     it('should return -1 when inputs are 1 and 1.8', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1, 1.8), -1);
+      expect(calculateNumber('SUBTRACT', 1, 1.8)).to.equal(-1);
     });
 
     it('should return 0 when inputs are 1.5 and 1.5', () => {
-      assert.strictEqual(calculateNumber('SUBTRACT', 1.5, 1.5), 0);
+      expect(calculateNumber('SUBTRACT', 1.5, 1.5)).to.equal(0);
     });
   });
 
   describe('DIVIDE', () => {
     it('should return 2 when inputs are 4 and 1.8', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 4, 1.8), 2);
+      expect(calculateNumber('DIVIDE', 4, 1.8)).to.equal(2);
     });
 
     it('should return "Error" when inputs are 1.5 and 0', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.5, 0), 'Error');
+      expect(calculateNumber('DIVIDE', 1.5, 0)).to.equal('Error');
     });
 
     it('should return "Error" when inputs are 1.5 and 0.4', () => {
-      assert.strictEqual(calculateNumber('DIVIDE', 1.5, 0.4), 'Error');
+      expect(calculateNumber('DIVIDE', 1.5, 0.4)).to.equal('Error');
     });
   });
 
   it('should throw an error for invalid type', () => {
-    assert.throws(() => {
+    expect(() => {
       calculateNumber('MULTIPLY', 1, 3.7);
-    }, /Invalid type/);
+    }).to.throw('Invalid type');
   });
 });
