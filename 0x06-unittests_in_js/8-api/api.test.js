5bc21bc954faac8a1db2fd97ef8a082278d23c4e
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-25 23:26:13 +0300

    Files

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,38 +1,14 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('./api');
-const expect = chai.expect;
+const request = require('request');
+const { expect } = require('chai');
 
-chai.use(chaiHttp);
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-describe('Index page', function() {
-  it('should return status code 200', function(done) {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-
-  it('should return the correct message', function(done) {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
-  });
-
-  it('should have the correct content type', function(done) {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res).to.have.header('content-type', 'text/html; charset=utf-8');
-        done();
-      });
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
   });
 });
